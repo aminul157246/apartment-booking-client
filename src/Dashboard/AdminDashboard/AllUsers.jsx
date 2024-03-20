de844139ed9d4560ed044bb1306d8a8649b2efce
aminul157246 <aminul.islam.157246@gmail.com>
2024-03-20 23:00:04 +0600

    show all users data in admin route

--- src/Dashboard/AdminDashboard/AllUsers.jsx
+++ src/Dashboard/AdminDashboard/AllUsers.jsx
@@ -1,10 +1,124 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
+
+    const axiosSecure = useAxiosSecure()
+
+    const { refetch, data: users = [] } = useQuery({
+        queryKey: ['user'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/users')
+            console.log(res.data);
+            return res.data;
+
+        }
+    })
+
+
+    const handleMakeAdmin = (id) => {
+        axiosSecure.patch(`/users/admin/${id}`)
+            .then(res => {
+                console.log(res.data);
+                if (res.data.modifiedCount > 0) {
+                    Swal.fire({
+                        title: "Successful!",
+                        text: `You are now admin`,
+                        icon: "success"
+                    });
+                }
+            })
+    }
+
+
+
+    const handleDeleteUser = (id) => {
+        refetch()
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+
+            if (result.isConfirmed) {
+
+                axiosSecure.delete(`/users/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            refetch()
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                        console.log(res.data);
+                    })
+
+            }
+        });
+    }
+
     return (
         <div>
-            AllUsers
-AllUsers
+            <h2 className="text-4xl text-center">All User</h2>
+            <h3>Total User : {users.length}</h3>
+
+
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                #
+                            </th>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Email Color</th>
+                            <th>Update</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            users.map((user, idx) => <tr key={user._id}>
+                                <th>
+                                    {idx + 1}
+                                </th>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={user.photo} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>
+                                    {
+                                        user.role === 'admin' ? 'Admin' : <button onClick={() => handleMakeAdmin(user._id)}><FaUsers /></button>
+                                    }
+                                </td>
+                                <td><button onClick={() => handleDeleteUser(user._id)}>X</button></td>
+                            </tr>)
+                        }
+
+                    </tbody>
+
+
+                </table>
+            </div>
+
         </div>
     );
 };
--- src/Pages/layout/Dashboard.jsx
+++ src/Pages/layout/Dashboard.jsx
@@ -9,42 +9,42 @@ const Dashboard = () => {
 
     return (
         <div className="flex">
-<Helmet><title>New Home || Dashboard</title></Helmet>
+            <Helmet><title>New Home || Dashboard</title></Helmet>
             <div className="w-1/4 bg-gray-400 h-[700px]">
-              {
-                isAdmin ?   <>
-                <li>
-                    <NavLink to={'/dashboard/adminHome'}> Admin Home </NavLink>
-                </li>
+                {
+                    isAdmin ? <>
+                        <li>
+                            <NavLink to={'/dashboard/adminHome'}> Admin Home </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={'/dashboard/allUsers'}>  All Users </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={'/dashboard/addItems'}>  Add Items </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={'/dashboard/manageItems'}> Manage Items </NavLink>
+                        </li>
+                        <li>
+                            {/* <NavLink to={'/dashboard/manageBooking'}> Manage Booking </NavLink> */}
+                        </li>
 
-                <li>
-                    <NavLink to={'/dashboard/addItems'}>  Add Items </NavLink>
-                </li>
-                <li>
-                    <NavLink to={'/dashboard/manageItems'}> Manage Items </NavLink>
-                </li>
-                <li>
-                    <NavLink to={'/dashboard/manageBooking'}> Manage Booking </NavLink>
-                </li>
-                <li>
-                    <NavLink to={'/dashboard/allUsers'}>  All Users </NavLink>
-                </li>
-            </>
-                : 
-                <>
-                <li><NavLink to={'/dashboard/memberProfile'}>Profile</NavLink></li>
-                <li><NavLink to={'/dashboard/agreement'}>Agreement</NavLink></li>
-                <li><NavLink to={'/dashboard/payment'}>payment</NavLink></li>
-                {/* <li><NavLink to={'/dashboard/paymentHistory'}>Payment History</NavLink></li> */}
-                <li><NavLink to={'/dashboard/announcement'}>Announcement</NavLink></li>
-                <div className="divider"></div>
-                <li><NavLink to={'/'}>Home</NavLink></li>
-                </>
-              }
+                    </>
+                        :
+                        <>
+                            <li><NavLink to={'/dashboard/memberProfile'}>Profile</NavLink></li>
+                            <li><NavLink to={'/dashboard/agreement'}>Agreement</NavLink></li>
+                            <li><NavLink to={'/dashboard/payment'}>payment</NavLink></li>
+                            {/* <li><NavLink to={'/dashboard/paymentHistory'}>Payment History</NavLink></li> */}
+                            <li><NavLink to={'/dashboard/announcement'}>Announcement</NavLink></li>
+                            <div className="divider"></div>
+                            <li><NavLink to={'/'}>Home</NavLink></li>
+                        </>
+                }
             </div>
 
             <div className="w-3/4 bg-gray-200 h-[700px]">
-<Outlet/>
+                <Outlet />
             </div>
 
         </div>
