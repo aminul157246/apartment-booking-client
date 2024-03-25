9a72058acb562ebb5b2f29e93d99a51fe2d1fa93
aminul157246 <aminul.islam.157246@gmail.com>
2024-03-25 09:18:28 +0600

    added drawer in dashboard

--- src/Pages/layout/Dashboard.jsx
+++ src/Pages/layout/Dashboard.jsx
@@ -1,18 +1,36 @@
 import { Helmet } from "react-helmet";
 import { NavLink, Outlet } from "react-router-dom";
 import useAdmin from "../../hooks/useAdmin";
+import { useState } from "react";
+import 'react-modern-drawer/dist/index.css';
+import Drawer from 'react-modern-drawer';
+import { FiAlignJustify } from "react-icons/fi";
 
 const Dashboard = () => {
 
 
     const [isAdmin] = useAdmin();
-    // console.log(isAdmin);
+    console.log(isAdmin);
 
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDrawer = () => {
+        setIsOpen((prevState) => !prevState);
+    }
 
     return (
         <div className="flex">
             <Helmet><title>New Home || Dashboard</title></Helmet>
-            <div className="w-1/5 p-12  bg-gray-300">
+
+            <div className="lg:ml-24 ml-8 mt-8 lg:mt-16 text-4xl">
+            <button onClick={toggleDrawer}><FiAlignJustify/></button>
+            </div>
+            <Drawer
+                    open={isOpen}
+                    onClose={toggleDrawer}
+                    direction="left"
+                >
+            <div className="w-1/4 p-4 lg:p-12">
                 {
                     isAdmin ? <div className="list-none min-h-screen space-y-2">
 
@@ -99,7 +117,9 @@ const Dashboard = () => {
                 }
             </div>
 
-            <div className="w-4/5 bg-gray-100 p-12 ">
+            </Drawer>
+
+            <div className="w-3/4  lg:mt-12 ml-8 lg:ml-40 bg-gray-100 p-8 ">
                 <Outlet />
             </div>
 
